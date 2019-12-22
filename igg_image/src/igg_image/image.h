c995b7acf4e101938a9992b0464822b552cb6fd0
jnguy707 <jeremy.n.707@live.com>
2019-12-21 19:47:25 -0800

    Re-do of fourth commit

--- igg_image/src/igg_image/image.h
+++ igg_image/src/igg_image/image.h
@@ -7,11 +7,28 @@ namespace igg {
 class Image {
  public:
   ///////////////////// Create the public interface here ///////////////////////
+// Constructor(s)
+	Image();
+	Image(int rows, int cols): rows_{rows}, cols_{cols} {
+	data_.reserve(rows * cols +1);	
+	}
+// Getter
+		int rows() const {return rows_;}
+		int cols() const {return cols_;}
+// Declare of .at function
+	// Reference the value
+		const int at(int row,int col) const
+	// Set the value
+		int at(int row,int col);
+// Declare  Reading and Writing to Disk
+		bool FillFromPgm(const std::string& file_name);
+		void WriteToPgm(const std::string& file_name);
+
  private:
-  int rows_ = 0;
-  int cols_ = 0;
-  int max_val_ = 255;
-  std::vector<int> data_;
+	int rows_ = 0;
+	int cols_ = 0;
+	int max_val_ = 255;
+	std::vector<int> data_;
 };
 
 }  // namespace igg
